d6f552caff01734728d62330ce3b6047629c021a
Paul Lynch <pinnacleluminary12@gmail.com>
2026-02-15 17:53:38 +0000

    adjust reg ratio

--- scripts/text_trainer.py
+++ scripts/text_trainer.py
@@ -302,7 +302,7 @@ def main():
     )
 
     parser.add_argument(
-        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1.1001
+        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1.15
     )
     parser.add_argument(
         "--wandb-mode",
