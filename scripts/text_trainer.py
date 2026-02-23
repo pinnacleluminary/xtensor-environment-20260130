bd815aaadd56f14277d5e6a68ecd3a65d3202563
Paul Lynch <pinnacleluminary12@gmail.com>
2026-02-23 12:38:49 +0000

    udpate

--- scripts/text_trainer.py
+++ scripts/text_trainer.py
@@ -302,7 +302,7 @@ def main():
     )
 
     parser.add_argument(
-        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1.0312
+        "--reg-ratio", type=float, help="Reg ratio to use for training", default=0.950135
     )
     parser.add_argument(
         "--wandb-mode",
